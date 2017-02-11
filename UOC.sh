42ee53e01d9f2d150df937a741d48fa54cc90ecc
L0sted <theext4@yandex.ru>
2017-02-11 14:42:01 +0300

    Update UOC.sh

--- UOC.sh
+++ UOC.sh
@@ -13,14 +13,15 @@ echo -n 0 > /sys/bus/platform/devices/sunxi_usb_udc/otg_role ; modprobe -r {g_ma
 }
 function apply_choise {
 case $choice in
+ 0)
+    prepare_to_switch;;
  1)
     prepare_to_switch ; modprobe g_ether iProduct=opi iManufacturer=Losted dev_addr=62:ef:11:22:11:22 host_addr=ea:c9:35:d4:66:87 use_eem=0 ; echo -n 2 > /sys/bus/platform/devices/sunxi_usb_udc/otg_role;;
  2)
     prepare_to_switch ; modprobe g_mass_storage file=$MassStorage1 stall=0 ; echo -n 2 > /sys/bus/platform/devices/sunxi_usb_udc/otg_role;;
  3)
     prepare_to_switch ; modprobe g_mass_storage file=$MassStorage2 stall=0 ; echo -n 2 > /sys/bus/platform/devices/sunxi_usb_udc/otg_role;;
- 4)
-    prepare_to_switch;;
+
 esac
 }
 DIALOG=${DIALOG=dialog}
@@ -28,10 +29,11 @@ tempfile=`mktemp 2>/dev/null` || tempfile=/tmp/test$$
 trap "rm -f $tempfile" 0 1 2 5 15
 $DIALOG --clear --title "OTG" \
         --menu "Choice OTG mode:" 12 50 10 \
-        "1"  "Usb Ethernet" \
+	"0"  "Disable OTG" \
+	"1"  "Usb Ethernet" \
+	"2"  "Serial" \
         "2"  "Mass storage device 1" \
-        "3"  "Mass storage device 2" \
-	"4"  "Disable OTG" 2> $tempfile
+        "3"  "Mass storage device 2" 2> $tempfile
 
 retval=$?
 choice=`cat $tempfile`
